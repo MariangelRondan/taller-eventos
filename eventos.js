ffbd5fb87f410448f758f48bff808138a93a4762
MariangelRondan <rondanmariangel@gmail.com>
2023-08-07 12:38:07 -0300

    guardo en rama solucion-click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,12 @@
 let div = document.querySelector("#div");
 
 div.addEventListener("click", () => {alert("Hola! Soy el div.")})
+
+
+let boton = document.querySelector("button");
+
+boton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    alert('Hola!')
+   
+})
